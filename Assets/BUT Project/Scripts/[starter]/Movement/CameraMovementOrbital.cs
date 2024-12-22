20884dae22c726cd10b125ef1df324a71e083987
Rackyrr <116524047+Rackyrr@users.noreply.github.com>
2024-12-22 16:21:51 +0100

    Move the camera around the map

--- Assets/BUT Project/Scripts/[starter]/Movement/CameraMovementOrbital.cs
+++ Assets/BUT Project/Scripts/[starter]/Movement/CameraMovementOrbital.cs
@@ -8,30 +8,53 @@ namespace BUT
     {
         [SerializeField]
         float m_SpeedRotation;
+
         public float SpeedRotation => Mathf.Deg2Rad * m_SpeedRotation * Time.deltaTime;
+
+        [SerializeField]
+        float m_SpeedFactorGamepad = 2.0f;
+
+        public float SpeedFactorGamepad => m_SpeedFactorGamepad;
 
         [SerializeField]
         Vector2 m_RotationXLimits;
 
         Vector3 m_CurrentRotation;
 
-        private void Start()
-        {
-            // init rotation
+        private void Start(){
+            // Init rotation
             m_CurrentRotation = transform.rotation.eulerAngles;
         }
 
-        // rotate when input received
-        public void Rotate(InputAction.CallbackContext _context)
-        {
-            // add rotation
-            m_CurrentRotation.y += _context.ReadValue<Vector2>().x * SpeedRotation;
-            m_CurrentRotation.x += -_context.ReadValue<Vector2>().y * SpeedRotation;
-            // clamp rotation
-            m_CurrentRotation.x = Mathf.Clamp(m_CurrentRotation.x, m_RotationXLimits.x, m_RotationXLimits.y);
-            
-            // apply rotation
-            transform.rotation = Quaternion.Euler(m_CurrentRotation.x, m_CurrentRotation.y, m_CurrentRotation.z);
+        private void Update(){
+            // Vérifie si un Gamepad est connecté
+            if (Gamepad.current != null) {
+
+                // Lecture de l'input du joystick droit
+                Vector2 input = Gamepad.current.rightStick.ReadValue();
+                RotateCamera(input, SpeedFactorGamepad);
+            }
+        }
+
+        private void RotateCamera(Vector2 input, float speedFactor){
+            // Vérifie si l'input est significatif
+            if (input.magnitude > 0.1f){
+                // Ajout de rotation
+                m_CurrentRotation.y += input.x * SpeedRotation * speedFactor; 
+                m_CurrentRotation.x += -input.y * SpeedRotation * speedFactor;
+
+                // Limiter la rotation sur l'axe X
+                m_CurrentRotation.x = Mathf.Clamp(m_CurrentRotation.x, m_RotationXLimits.x, m_RotationXLimits.y);
+                
+                // Appliquer la rotation
+                transform.rotation = Quaternion.Euler(m_CurrentRotation.x, m_CurrentRotation.y, m_CurrentRotation.z);
+            }
+        }
+
+        // Callback pour l'input system : surtout pour la souris
+        public void Rotate(InputAction.CallbackContext _context){
+            Vector2 input = _context.ReadValue<Vector2>();
+            RotateCamera(input, 5.0f);
         }
     }
 }
