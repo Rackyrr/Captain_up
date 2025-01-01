0884e9a139f9acf3c5e9df2c67eb401743c7720f
Robin <114415282+sandevistan14@users.noreply.github.com>
2025-01-02 00:26:28 +0100

    add UI, pause and victory and trophy

---
+++ Assets/Scripts/HUD/UiManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiManager : MonoBehaviour
+{
+    public GameObject HUDGame;
+    public GameObject HUDEndGame;
+    public GameObject PauseMenu;
+
+    public bool GameFinish = false;
+    // private bool isPaused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowUIGame();
+        ResumeGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!HUDEndGame.activeSelf)
+        {
+            {
+                if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.JoystickButton7))
+                {
+                    if (PauseMenu.activeSelf)
+                    {
+                        ShowUIGame();
+                        ResumeGame();
+                    }
+                    else
+                    {
+                        ShowPauseMenu();
+                        PauseGame();
+                    }
+                }
+            }
+        }
+    }
+
+    public void goBackToMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+    }
+
+    public void ShowUIGame()
+    {
+        HUDGame.SetActive(true);
+        HUDEndGame.SetActive(false);
+        PauseMenu.SetActive(false);
+        ResumeGame();
+    }
+
+    public void ShowEndGame()
+    {
+        GameFinish = true;
+        HUDEndGame.SetActive(true);
+        HUDGame.SetActive(false);
+    }
+
+    public void ShowPauseMenu()
+    {
+        HUDGame.SetActive(false);
+        HUDEndGame.SetActive(false);
+        PauseMenu.SetActive(true);
+        PauseGame();
+    }
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0f; // Met le temps du jeu à 0 (pause)
+    }
+
+    private void ResumeGame()
+    {
+        Time.timeScale = 1f; // Rétablit le temps normal
+    }
+}
---
+++ Assets/Scripts/Object/Tronphee.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tronphee : MonoBehaviour
+{
+
+    public GameObject onCollectEffect;
+    public AudioClip tronpheeSound;
+    public UiManager _UiManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0.1f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Instantiate(onCollectEffect, transform.position, transform.rotation);
+        if (tronpheeSound != null)
+        {
+            AudioSource.PlayClipAtPoint(tronpheeSound, Vector3.zero);
+        }
+        _UiManager.ShowEndGame();
+    }
+}
